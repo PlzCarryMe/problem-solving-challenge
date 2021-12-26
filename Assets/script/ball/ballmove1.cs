9a300b3d30c1e1f4a5e8238e1ca7658efa227623
PlzCarryMe <riccowiguna1@gmail.com>
2021-12-27 00:00:34 +0700

    v1

---
+++ Assets/script/ball/ballmove1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmove1 : MonoBehaviour
+{
+    Rigidbody ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+        ball.AddForce(new Vector3(1, 0, 1).normalized * 500f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
