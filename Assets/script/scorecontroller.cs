6e7c3362ec7c3fa32e5fd9bb64dab21ce98c3a5b
PlzCarryMe <riccowiguna1@gmail.com>
2021-12-27 23:55:20 +0700

    v7

---
+++ Assets/script/scorecontroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scorecontroller : MonoBehaviour
+{
+    public static int score;
+    public Text textscore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        textscore = GameObject.Find("score").GetComponent<Text>();
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "ball")
+        {
+            Destroy(gameObject);
+            score++;
+            textscore.text = score.ToString();
+        }
+        
+    }
+
+}
