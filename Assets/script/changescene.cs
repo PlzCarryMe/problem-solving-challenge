80d7fe3826dc14ccf625b21e48fa14fbc1db464a
PlzCarryMe <riccowiguna1@gmail.com>
2021-12-28 23:15:46 +0700

    v10

---
+++ Assets/script/changescene.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class changescene : MonoBehaviour
+{
+    public void level0()
+    {
+        SceneManager.LoadScene("sceneselector");
+    }
+    public void level1()
+    {
+        SceneManager.LoadScene("problem 1");
+    }
+    public void level2()
+    {
+        SceneManager.LoadScene("problem 2");
+    }
+    public void level3()
+    {
+        SceneManager.LoadScene("problem 3");
+    }
+    public void level4()
+    {
+        SceneManager.LoadScene("problem 4");
+    }
+    public void level5()
+    {
+        SceneManager.LoadScene("problem 5");
+    }
+    public void level6()
+    {
+        SceneManager.LoadScene("problem 6");
+    }
+    public void level7()
+    {
+        SceneManager.LoadScene("problem 7");
+    }
+    public void level8()
+    {
+        SceneManager.LoadScene("problem 8");
+    }
+    public void level9()
+    {
+        SceneManager.LoadScene("problem 9");
+    }
+    public void level10()
+    {
+        SceneManager.LoadScene("problem 10");
+    }
+}
